f26d7499695e997b9411df051e75c8a64a1dccef
thienlpn <thienlpn0812@gmail.com>
2023-06-27 17:41:14 +0700

    refactor: - use port 8080 - use `ipfs-core` instead of `ipfs-http-client` - use esm

--- index.js
+++ index.js
@@ -1,52 +1,50 @@
-const ipfsClient = require('ipfs-http-client')
-const express = require('express')
-const bodyParser = require('body-parser')
-const fileUpload = require('express-fileupload')
-const fs = require('fs')
+import express from "express";
+import bodyParser from "body-parser";
+import fileUpload from "express-fileupload";
+import fs from "fs";
 
-const ipfs = new ipfsClient({host:'localhost', port: '5001', protocol:'http'})
-const app = express()
+import * as IPFS from "ipfs-core";
 
-app.set('view engine','ejs')
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(fileUpload())
+const ipfs = await IPFS.create();
+const app = express();
 
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
 
-app.get('/', (req,res) => {
-    res.render('home')
-    console.log('home hit')
-})
+app.get("/", (req, res) => {
+  res.render("home");
+  console.log("home hit");
+});
 
-app.post('/upload', (req, res) => {
-     const file = req.files.file
-     const fileName = req.body.fileName
-     const filePath =   'files/' + fileName
+app.post("/upload", (req, res) => {
+  const file = req.files.file;
+  const fileName = req.body.fileName;
+  const filePath = "files/" + fileName;
 
-     file.mv(filePath, async (err) => {
-         if (err) {
-            console.log('error: failed to download the file.')
-            return res.status(500).send(err)
-         }
-     const fileHash = await addFile(fileName, filePath)
-     fs.unlink(filePath, (err) =>{
-         if (err) console.log(err)
-     })  
-         
-     res.render('upload', {fileName, fileHash})
-     })
-})
+  file.mv(filePath, async (err) => {
+    if (err) {
+      console.log("error: failed to download the file.");
+      return res.status(500).send(err);
+    }
+    const fileHash = await addFile(fileName, filePath);
+    fs.unlink(filePath, (err) => {
+      if (err) console.log(err);
+    });
+
+    res.render("upload", { fileName, fileHash });
+  });
+});
 
 const addFile = async (fileName, filePath) => {
-    const file = fs.readFileSync(filePath)
-   const fileAdded = await ipfs.add({path:fileName, content:file})
-  
+  const file = fs.readFileSync(filePath);
+  const fileAdded = await ipfs.add({ path: fileName, content: file });
 
+  const fileHash = fileAdded.cid;
+  console.log(fileAdded);
+  return fileHash;
+};
 
-   const fileHash = fileAdded.cid
-  console.log(fileAdded)
-    return fileHash
-}
-
-app.listen(3000,'127.0.0.1', () => {
-    console.log('Server is running on port 3000')
-})
+app.listen(8080, "127.0.0.1", () => {
+  console.log("Server is running on port 8080");
+});
